b35dfd5ea27dd34a7d564beef3ec9f899b26e69c
MOHYIDDINEDEBIH80088 <al80088@alunos.utad.pt>
2025-04-24 15:08:54 +0100

    Add files via upload

---
+++ API_PROJECT/API_PROJECT/Product.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace API_PROJECT
+{
+    public class Product
+    {
+       // public int ID_Produto { get; set; }
+        public string? Codigo_Peca { get; set; }
+        public DateTime Data_Producao { get; set; }
+        public TimeSpan Hora_Producao { get; set; }
+        public int Tempo_Producao { get; set; }
+
+       // public string? TestResult { get; set; }
+
+
+        public void WriteItem(SqlCommand cmd,
+         string Codigo_Peca,
+        DateTime Data_Producao, TimeSpan Hora_Producao, int Tempo_Producao)
+        {
+           // cmd.Parameters.Add("@ID_Produto", System.Data.SqlDbType.Int).Value = ID_Produto;
+            cmd.Parameters.Add("@Codigo_Peca", System.Data.SqlDbType.VarChar).Value = Codigo_Peca;
+            cmd.Parameters.Add("@Data_Producao", System.Data.SqlDbType.Date).Value = Data_Producao;
+            cmd.Parameters.Add("@Hora_Producao", System.Data.SqlDbType.Time).Value = Hora_Producao;
+            cmd.Parameters.Add("@Tempo_Producao", System.Data.SqlDbType.Int).Value = Tempo_Producao;
+           // cmd.Parameters.Add("@TestResult", System.Data.SqlDbType.VarChar).Value = TestResult;
+
+
+        }
+    }
+}
---
+++ API_PROJECT/API_PROJECT/Teste.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace API_PROJECT
+{
+    public class Teste
+    {
+        //public int ID_Teste { get; set; }
+        public int ID_Produto { get; set; }
+        public string? Codigo_Resultado { get; set; }
+        public DateTime Data_Teste { get; set; }
+
+        public void WriteItem(SqlCommand cmd,
+        int ID_Produto,
+       string Codigo_Resultado, DateTime Data_Teste)
+        {
+           // cmd.Parameters.Add("@ID_Teste", System.Data.SqlDbType.VarChar).Value = ID_Teste;
+            cmd.Parameters.Add("@ID_Produto", System.Data.SqlDbType.Int).Value = ID_Produto;
+            cmd.Parameters.Add("@Codigo_Resultado", System.Data.SqlDbType.VarChar).Value = Codigo_Resultado;
+            cmd.Parameters.Add("@Data_Teste", System.Data.SqlDbType.Date).Value = Data_Teste;
+        
+        }
+    }
+}
